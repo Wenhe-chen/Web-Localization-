cafe480dc64066e25917f23d896fb6bbb4824507
Wenhe-chen <147887601+Wenhe-chen@users.noreply.github.com>
2023-10-13 16:39:13 -0700

    Add files via upload

---
+++ Greeting and Calculate dunction.py
@@ -0,0 +1,29 @@
+# Written by Summer Chen
+print("Hello World")
+print("what is your first name, baby?")
+firstname = input()
+print("Nice 2 meet you, " + firstname)
+numb1 = input("what is your favorite number, dear?")
+numb2 = input("And another number?")
+sum = float(numb1) + float(numb2)
+product = float(numb1) * float(numb2)
+quotient = float(numb1) / float(numb2)
+subs = float(numb1) - float(numb2)
+exponent = float(numb1)**float(numb2)
+Integer = float(numb1)//float(numb2)
+print("the sum of those two numbers are" + str(sum))
+print("the product of those two numbers are" + str(product))
+print("the quotient of those two numbers are" + str(quotient))
+print("the substraction of those two numbers are" + str(subs))
+print("the exponent of those two numbers are" + str(exponent))
+print("the integer of those two numbers are" + str(Integer))
+numb3 = numb1
+answer = (float(numb3))**2*3.14
+print("the area of the numb1 as a radius is" + str(answer))
+numb4 = numb2
+answer1 = (float(numb2))**2*3.14
+print("the area of the numb2 as a radius is" + str(answer1))
+answer2 =(float(numb3))**3*3.14*0.75
+print("the volume of the numb1 as a radius is" +str(answer2))
+answer3 = (float(numb4))**3*3.14*0.75
+print("the volume of the numb2 as a radius is" +str(answer3))# Write your code here :-)
